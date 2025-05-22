ebf633f2024bbd9c663de5a874915539c102cf7e
OmiKat <ompraveer@gmail.com>
2025-05-22 23:43:50 +0530

    .

---
+++ src/main/java/com/example/Ecommerce/Backend/Controller/ProductController.java
@@ -0,0 +1,51 @@
+package com.example.Ecommerce.Backend.Controller;
+
+import com.example.Ecommerce.Backend.Model.Product;
+import com.example.Ecommerce.Backend.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("http://localhost:5173/")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts(){
+        List<Product> products  = service.getAllProducts();
+        if(products != null){
+            return new ResponseEntity<>(products, HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id){
+        Product product = service.getProductById(id);
+        if(product != null){
+            return new ResponseEntity<>(product, HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addproduct(@RequestPart Product product , @RequestPart MultipartFile imageFile){
+        Product savedProduct = null;
+        try {
+            savedProduct = service.addProduct(product, imageFile);
+            return new ResponseEntity<>(savedProduct,HttpStatus.CREATED);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
---
+++ src/main/java/com/example/Ecommerce/Backend/Service/ProductService.java
@@ -0,0 +1,35 @@
+package com.example.Ecommerce.Backend.Service;
+
+import com.example.Ecommerce.Backend.Model.Product;
+import com.example.Ecommerce.Backend.Repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo repo;
+
+    public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile image) throws IOException {
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+
+        return repo.save(product);
+
+    }
+}
